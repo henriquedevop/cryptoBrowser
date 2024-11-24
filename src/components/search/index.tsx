506508888285ee0e3090af00a71dc8b9e634ab9c
henriquedevop <wymuxgh@gmail.com>
2024-11-24 17:09:48 -0300

    feat: add api connection, add data in home. improve style

--- src/components/search/index.tsx
+++ src/components/search/index.tsx
@@ -1,13 +1,30 @@
 import { MagnifyingGlassIcon } from "@heroicons/react/16/solid";
+import { FormEvent, useState } from "react";
+import { useNavigate } from "react-router";
 
 export function Search() {
+
+  const [input, setInput] = useState("")
+
+  const navigate = useNavigate()
+
+  function handleSubmit(e:FormEvent) {
+    e.preventDefault()
+
+    if(input === "") return
+
+    navigate(`/detail/${input.trim()}`)
+  }
+  
   return (
-    <form className="flex items-center">
+    <form onSubmit={handleSubmit} className="flex items-center">
       <div className="relative flex gap-5">
         <input
           type="text"
           placeholder="Search"
           className="md:w-64 md:focus:w-80 bg-gray-100 rounded-md pl-10 pr-3 py-1 w-28 transition-all duration-300 ease-in-out focus:w-48 focus:pl-12"
+          value={input}
+          onChange={ (e) => setInput(e.target.value) }
         />
         <MagnifyingGlassIcon className="absolute left-3 top-1/2 transform -translate-y-1/2 h-5 w-5 text-slate-500" />
       </div>
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,67 @@
 import { Link } from "react-router";
+import { useEffect, useState } from "react";
+import api from "../../services/api";
+
+interface CryptoProps {
+  changePercent24Hr: string,
+  explorer: string,
+  id: string,
+  marketCapUsd: string,
+  maxSupply: string,
+  name: string,
+  priceUsd: string,
+  rank: string,
+  supply: string,
+  symbol: string,
+  volumeUsd24Hr: string,
+  vwap24Hr: string,
+  formatedPrice?: string,
+  formatedMarket?: string,
+  formatedValue?: string,
+}
 
 export function Home() {
+
+  const [cryptos, setCryptos] = useState<CryptoProps[]>([])
+
+  useEffect(() => {
+    getData()
+  },[])
+
+  async function getData() {
+    try {
+      
+      const response = await api.get("assets", {params: { limit: 10, offset: 0 }});
+
+      const cryptoData = response.data.data
+      
+      const priceFormatter = Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD"
+      });
+
+      const priceCompacter = Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+        notation: "compact"
+      });
+
+      const formatedResult = cryptoData.map((item: CryptoProps) => ({
+        ...item,
+        formatedPrice: priceFormatter.format(Number(item.priceUsd)),
+        formatedMarket: priceCompacter.format(Number(item.marketCapUsd)),
+        formatedValue: priceCompacter.format(Number(item.volumeUsd24Hr)),
+      }))
+
+      setCryptos(formatedResult);
+    } catch (error) {
+      console.error("Erro ao buscar dados:", error);
+    }
+  }
+
   return (
-    <main className="p-5">
-      <table className="min-w-full table-auto border-collapse bg-white shadow-lg rounded-lg overflow-hidden">
+    <main className="p-5 w-full">
+      <table className="border-separate border-spacing-y-4 m-0 p-0 w-full table-auto">
         <thead>
           <tr className="text-black border-b-[1.5px]">
             <th scope="col" className="px-4 py-2 text-left">Moeda</th>
@@ -15,20 +73,34 @@ export function Home() {
         </thead>
         
         <tbody id="tbody">
-          <tr className="border-b hover:bg-gray-100">
-            <td className="px-4 py-2">
-              <Link to="detail/btc" className="text-blue-500 font-bold hover:underline">
-                <span>Bitcoin | BTC</span>
+          {cryptos.length > 0 && cryptos.map((item) => (
+          <tr className="bg-gray-50" key={item.id}>
+            <td className="px-4 py-4 text-left">
+              <img
+              alt={`crypto logo: ${item.name}`}
+              src={`https://assets.coincap.io/assets/icons/${item.symbol.toLocaleLowerCase()}@2x.png`}
+              className="w-6"
+              />
+              <Link to={`/detail/${item.id}`} className="font-bold hover:underline ">
+                <span className=""><span className="text-sm text-zinc-600">{item.rank}</span> {item.name} <span className="text-zinc-400 text-xs">{item.symbol}</span></span>
               </Link>
             </td>
-            <td className="hidden md:block px-4 py-2" data-label="market value">1T</td>
-            <td className="px-4 py-2" data-label="price">58.000$</td>
-            <td className="hidden md:block px-4 py-2" data-label="vol">54B</td>
-            <td className="px-4 py-2" data-label="change24">16%</td>
+            <td className=" hidden md:block px-4 py-4" data-label="marketvalue">{item.formatedMarket}</td>
+            <td className="px-4 py-2 font-medium" data-label="price">{item.formatedPrice}</td>
+            <td className="hidden md:block px-4 py-7" data-label="vol">{item.formatedValue}</td>
+            <td 
+            className={`px-4 py-2 font-bold
+            ${Number(item.changePercent24Hr) > 0 ? 'text-green-600' : 'text-red-600'}  
+            `} 
+            data-label="change24">
+            {Number(item.changePercent24Hr).toFixed(2)}
+            </td> 
           </tr>
+          ))}
         </tbody>
         
       </table>
+      <button className="bg-green-500 rounded-md p-1 text-white font-bold mt-3">Buscar mais...</button>
     </main>
   );
 }
